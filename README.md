92dcfc65c50e7a7d12b01eb7637045ad79e61616
Zach Pomerantz <zmp@umich.edu>
2015-03-21 02:03:58 -0700

    (feat) Add confidence

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This is a fork of [cwilso's PitchDetect](https://github.com/cwilso/PitchDetect).
 It changes the API from a demo application to a usable interface:
 
 ```
-pitcher(); // { pitch: Integer || undefined, note: String || undefined, detune: Integer || undefined }
+pitcher(); // { pitch: Number || undefined, note: String || undefined, detune: Integer || undefined, confidence: Number || undefined }
 ```
 
-When the script is run in a browser, the user will automagically be prompted to use their microphone. Once this is done, calling `pitcher()` will return the `pitch`, `note`, and `detune` - if detectable.
+When the script is run in a browser, the user will automagically be prompted to use their microphone. Once this is done, calling `pitcher()` will return the `pitch`, `note`, `detune`, and `confidence` level - if detectable.
--- pitcher.js
+++ pitcher.js
@@ -72,7 +72,6 @@ function gotStream(stream) {
     analyser = context.createAnalyser();
     analyser.fftSize = FFT_SIZE;
     source.connect(analyser);
-    //analyser.connect(context.destination);
 }
 
 function getPitch() {
@@ -81,76 +80,83 @@ function getPitch() {
 	analyser.getFloatTimeDomainData(buffer); // Alter buffer by reference
 	var ac = autoCorrelate(buffer, context.sampleRate);
 
- 	if (ac == -1) { return result(); }
+ 	if (ac == null) { return result(); }
      
-  var pitch = ac;
+  var pitch = ac.pitch;
   var note = noteFromPitch(pitch);
   var detune = centsOffFromPitch(pitch, note);
 
-  return result(pitch, note, detune);
+  return result(pitch, note, detune, ac.confidence);
 
-  function result(pitch, note, detune) {
+  function result(pitch, note, detune, confidence) {
     return {
       pitch: pitch,
       note: NOTE_STRINGS[note % 12],
       detune: detune,
+      confidence: confidence,
     };
   }
 }
 
 function autoCorrelate(buffer, sampleRate) {
-	var SIZE = buffer.length;
-	var MAX_SAMPLES = Math.floor(SIZE / 2);
-	var best_offset = -1;
-	var best_correlation = 0;
+	var MAX_SAMPLES = Math.floor(FFT_SIZE / 2);
+	var bestOffset = -1;
+	var bestCorrelation = 0;
 	var rms = 0;
 	var foundGoodCorrelation = false;
 	var correlations = new Array(MAX_SAMPLES);
+  var val;
 
-	for (var i=0;i<SIZE;i++) {
-		var val = buffer[i];
-		rms += val*val;
+	for (var i = 0; i < FFT_SIZE; i++) {
+		val = buffer[i];
+		rms += val * val;
 	}
-	rms = Math.sqrt(rms/SIZE);
-	if (rms<0.01) // not enough signal
-		return -1;
 
-	var lastCorrelation=1;
+	rms = Math.sqrt(rms / FFT_SIZE);
+	if (rms<0.01) { return null; } // Not enough signal
+
+	var lastCorrelation = 1;
 	for (var offset = MIN_SAMPLES; offset < MAX_SAMPLES; offset++) {
 		var correlation = 0;
 
-		for (var i=0; i<MAX_SAMPLES; i++) {
-			correlation += Math.abs((buffer[i])-(buffer[i+offset]));
+		for (var i=0; i < MAX_SAMPLES; i++) {
+			correlation += Math.abs((buffer[i]) - (buffer[i + offset]));
 		}
 		correlation = 1 - (correlation/MAX_SAMPLES);
-		correlations[offset] = correlation; // store it, for the tweaking we need to do below.
-		if ((correlation>0.9) && (correlation > lastCorrelation)) {
+		correlations[offset] = correlation; // Store it for a best fit tweak
+
+		if ((correlation>0.5) && (correlation > lastCorrelation)) {
 			foundGoodCorrelation = true;
-			if (correlation > best_correlation) {
-				best_correlation = correlation;
-				best_offset = offset;
+
+			if (correlation > bestCorrelation) {
+				bestCorrelation = correlation;
+				bestOffset = offset;
 			}
 		} else if (foundGoodCorrelation) {
-			// short-circuit - we found a good correlation, then a bad one, so we'd just be seeing copies from here.
-			// Now we need to tweak the offset - by interpolating between the values to the left and right of the
-			// best offset, and shifting it a bit.  This is complex, and HACKY in this code (happy to take PRs!) -
-			// we need to do a curve fit on correlations[] around best_offset in order to better determine precise
-			// (anti-aliased) offset.
+      /* Short-circuit: once we find one, the rest of the waveform will look the same.
+       * We need to interpolate between values to the left and right of the offset,
+       * to better approximate the waveform.
+       */ 
+			var shift = // TODO: Replace with better interpolating function
+        (correlations[bestOffset + 1] - correlations[bestOffset - 1]) / correlations[bestOffset]; 
+      var pitch = sampleRate / (bestOffset + (8 * shift));
 
-			// we know best_offset >=1, 
-			// since foundGoodCorrelation cannot go to true until the second pass (offset=1), and 
-			// we can't drop into this clause until the following pass (else if).
-			var shift = (correlations[best_offset+1] - correlations[best_offset-1])/correlations[best_offset];  
-			return sampleRate/(best_offset+(8*shift));
+			return {
+        pitch: pitch,
+        confidence: bestCorrelation,
+      };
 		}
+
 		lastCorrelation = correlation;
 	}
-	if (best_correlation > 0.01) {
-		// console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")")
-		return sampleRate/best_offset;
+	if (bestCorrelation > 0.01) {
+		return {
+      pitch: sampleRate / bestOffset,
+      confidence: bestCorrelation,
+    };
 	}
-	return -1;
-//	var best_frequency = sampleRate/best_offset;
+
+	return null;
 }
 
 function noteFromPitch(frequency) {
